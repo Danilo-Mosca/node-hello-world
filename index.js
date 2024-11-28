26afd95207378d8d39157722006dcc3556a6a5f7
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-28 15:38:58 +0100

    added console.log for argument istruction

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+// Semplice console log
+console.log("Hello Boolean");
+
+// Passaggio di tutti argomenti
+console.log(process.argv);
+
+// Lettura di un argomento specifico: in questo caso il terzo
+console.log(process.argv[2]);
+
