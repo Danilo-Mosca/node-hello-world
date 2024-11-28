91e78bf8d2a1c0369efe7e1fdffd4bc9c08eea48
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-28 17:17:13 +0100

    Fix empty parameter passing with default value

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ console.log(process.argv);
 
 // Lettura di un argomento specifico: in questo caso il terzo (equivalente al primo da me passato con la riga di comando da terminale)
 console.log(process.argv[2]);
+
+// Punto 5
+
+// Assegno alla costante parolaPassata l'eventuale parametro passato con la riga di comando
+let parolaPassata = process.argv[2];
+// Se non esiste gli assegno un valore di default: "Non hai passato nessun argomento"
+if (!parolaPassata) {
+    parolaPassata = "non hai passato nessun argomento";
+}
+// Visualizzo tutto in console log
+console.log(`Hello ${parolaPassata}`);
